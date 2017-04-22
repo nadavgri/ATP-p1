b97ecd930e69f8bf5c7f6d95f772a2951ce88df1
nadavgri <nadavgld@gmail.com>
2017-04-22 15:30:51 +0300

    commit push test

--- src/algorithms/mazeGenerators/Main.java
+++ src/algorithms/mazeGenerators/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
        int row = 10;
         int col = 40;
-       SimpleMazeGenerator s = new SimpleMazeGenerator();
-       // MyMazeGenerator s = new MyMazeGenerator();
+       //SimpleMazeGenerator s = new SimpleMazeGenerator();
+        MyMazeGenerator s = new MyMazeGenerator();
         Maze m = s.generate(col, row);
         m.print();
         System.out.println("Time: " + new MyMazeGenerator().measureAlgorithmTime(col, row));
